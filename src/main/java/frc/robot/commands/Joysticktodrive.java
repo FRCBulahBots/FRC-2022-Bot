c6101274ec77998246481c975f5c1653f4527119
mason pizza <mason.pizzalato@outlook.com>
2022-01-20 16:58:57 -0600

    Update to: Joysticktodrive

--- src/main/java/frc/robot/commands/Joysticktodrive.java
+++ src/main/java/frc/robot/commands/Joysticktodrive.java
@@ -5,31 +5,42 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.Drivetrain;
+
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
 public class Joysticktodrive extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final Drivetrain landing_gear;
+  private DoubleSupplier joyspeed;
+  private DoubleSupplier joyrotation;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public Joysticktodrive(Drivetrain subsystem) {
+  public Joysticktodrive(Drivetrain subsystem, DoubleSupplier joyspeed, DoubleSupplier joyrotation) {
     landing_gear = subsystem;
+    this.joyspeed=joyspeed;
+    this.joyrotation=joyrotation;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    landing_gear.arcadeDrive(joyspeed.getAsDouble(), joyrotation.getAsDouble());
+  }
 
   // Called once the command ends or is interrupted.
   @Override
