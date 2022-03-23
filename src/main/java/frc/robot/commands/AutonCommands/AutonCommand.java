3cb7fb3b2a8fe0c01e648d8384bc4dd81f4ead3f
Bulah_Drive_Laptop <rohan.durgum@pbstudents.com>
2022-03-23 16:40:13 +0300

    initial draft of AutonCommand

--- src/main/java/frc/robot/commands/AutonCommands/AutonCommand.java
+++ src/main/java/frc/robot/commands/AutonCommands/AutonCommand.java
@@ -1,6 +1,34 @@
 package frc.robot.commands.AutonCommands;
 
-public class AutonCommand {
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.ScheduleCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.Drivetrain;
+
+
+public class AutonCommand extends CommandBase {
+
+    private Drivetrain drive;
+    private StartEndCommand driveForward = new StartEndCommand(() -> drive.arcadeDrive(0.3, 0), () -> drive.arcadeDrive(0,0), drive);
+    private StartEndCommand turntoOneDirection = new StartEndCommand(() -> drive.arcadeDrive(0, 0.3), () -> drive.arcadeDrive(0,0), drive);
     
+    public AutonCommand(Drivetrain drive){
+        this.drive = drive;
+    }
+
+    @Override
+    public void initialize() {
+        //new SequentialCommandGroup(new WaitCommand(2), driveForward, turntoOneDirection, driveForward);
+
+        new SequentialCommandGroup(new WaitCommand(2),new ScheduleCommand(driveForward).withTimeout(2), new ScheduleCommand(turntoOneDirection).withTimeout(2), new ScheduleCommand(driveForward.withTimeout(2)));
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        //this.cancel();
+    }
 }
-    
